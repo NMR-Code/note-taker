0b9aff31186c17873a092dccd5ab1622eeccb6cc
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-05-22 12:19:18 -0400

    removed duplicate lines

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+//==============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Class for notes
+//==============================================================
 class Note {
     constructor(title, text) {
         this.title = title;
@@ -24,11 +26,8 @@ class Note {
     }
 }
 
-//Using Static Files, Body Parser, and JSON.
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 // Routes
+
 // =============================================================
 
 // Basic route that sends the user to the index page
