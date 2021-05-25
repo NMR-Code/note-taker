bc581cb3d6ec11317438f1cf3dc913686f7ee761
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-05-25 11:40:10 -0400

    finally

--- server.js
+++ server.js
@@ -31,22 +31,22 @@ class Note {
 
 // Basic route that sends the user to the index page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    fs.readFile(path.join(__dirname, "public/index.html"));
 });
 
 // Basic route that sends the user to the notes page
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    fs.readFile(path.join(__dirname, "public/notes.html"));
 });
 
 // Route that sends the user the db.json file
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "db.json"));
+    fs.readFile(path.join(__dirname, "/db/db.json"));
 });
 
 // Takes a JSON input with keys "title" and "text" and adds a new note object with that message to the db.json file
 app.post("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(error, response) {
         if (error) {
             console.log(error);
         }
@@ -60,7 +60,7 @@ app.post("/api/notes", function(req, res) {
         };
         notes.push(newNote);
         res.json(newNote);
-        fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(err) {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes, null, 2), function(err) {
             if (err) throw err;
         });
     });
@@ -69,7 +69,7 @@ app.post("/api/notes", function(req, res) {
 // Deletes the note object with requested id from the db.json file, returns the deleted note; if no such id exists returns false
 app.delete("/api/notes/:id", function(req, res) {
     const deleteId = req.params.id;
-    fs.readFile("db.json", "utf8", function(error, response) {
+    fs.readFile("/db/db.json", "utf8", function(error, response) {
         if (error) {
             console.log(error);
         }
@@ -81,7 +81,7 @@ app.delete("/api/notes/:id", function(req, res) {
             for (let i = 0; i < notes.length; i++) {
                 notes[i].id = i + 1;
             }
-            fs.writeFile("db.json", JSON.stringify(notes, null, 2), function(err) {
+            fs.writeFile("/db/db.json", JSON.stringify(notes, null, 2), function(err) {
                 if (err) throw err;
             });
         } else {
