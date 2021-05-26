5d98124ababf81ef03f51090afc966d61cbe63f1
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-05-26 15:17:25 -0400

    still....

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // Dependencies
-// =============================================================
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+
+// Package to create unique ids for the note objects.
+// =============================================================
+const { v4: uuidv4 } = require('uuid');
 
 // Sets up the Express App
 // =============================================================
@@ -34,18 +37,24 @@ app.get("/", function(req, res) {
     fs.readFile(path.join(__dirname, "public/index.html"));
 });
 
-// Basic route that sends the user to the notes page
+//Sends the notes.html file when the user hits the "/notes" path.
 app.get("/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(`${__dirname}/public/notes.html`);
 });
 
-// Route that sends the user the db.json file
+//Sends the information in the db.json file.
 app.get("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "/db/db.json"));
+    fs.readFile(`${__dirname}/db/db.json`, function(err, data) {
+        if (err) {
+            throw err;
+        } else {
+            res.send(data);
+        }
+    });
 });
 
 // Takes a JSON input with keys "title" and "text" and adds a new note object with that message to the db.json file
-app.post("/api/notes", function(req, res) {
+app.post("/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(error, response) {
         if (error) {
             console.log(error);
@@ -68,29 +77,31 @@ app.post("/api/notes", function(req, res) {
 
 // Deletes the note object with requested id from the db.json file, returns the deleted note; if no such id exists returns false
 app.delete("/api/notes/:id", function(req, res) {
-    const deleteId = req.params.id;
-    fs.readFile("/db/db.json", "utf8", function(error, response) {
-        if (error) {
-            console.log(error);
-        }
-        let notes = JSON.parse(response);
-        if (deleteId <= notes.length) {
-            // Method to remove an element from an array obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-            res.json(notes.splice(deleteId - 1, 1));
-            // Reassign the ids of all notes
-            for (let i = 0; i < notes.length; i++) {
-                notes[i].id = i + 1;
-            }
-            fs.writeFile("/db/db.json", JSON.stringify(notes, null, 2), function(err) {
-                if (err) throw err;
-            });
+    const id = req.params.id;
+    fs.readFile(`${__dirname}/db/db.json`, 'utf8', function(err, data) {
+        if (err) {
+            throw err;
         } else {
-            res.json(false);
+            console.log(data);
+            const db = [];
+            JSON.parse(data).forEach((item, i) => {
+                db.push(item);
+            });
+            db.forEach((item, i) => {
+                if (item.id === id) {
+                    db.splice(i, 1);
+                }
+            });
+            fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(db), function(err) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log("Note added to data base");
+                }
+            });
+            res.redirect("/api/notes");
         }
-
-
     });
-
 });
 
 // Starts the server to begin listening
